4e844a4306fb9748da1e4c177d7c3c6698dcda8f
GustavoHMDS <gusttavoh2602@aluno.ufsj.edu.br>
2025-02-07 19:26:03 -0300

    FileUsuarioDAO implementado

--- InterfaceGrafica/src/Persistencia/UsuarioDAO.java
+++ InterfaceGrafica/src/Persistencia/UsuarioDAO.java
@@ -2,8 +2,12 @@ package Persistencia;
 
 import Modelo.Usuario;
 
+import java.io.IOException;
+
 public interface UsuarioDAO {
     Usuario logInUsuario(String email, String password);
-    boolean registrarUsuario(Usuario usuario);
-    Usuario editarUsuario(Usuario usuario);
+    boolean registrarUsuario(String cpf, String nome, String dataNascimento, String email, String senha, String tipo) throws Exception;
+    boolean editarUsuario(Usuario usuario, String[] dados);
+    boolean eliminarUsuario(String email);
+    void adminFS() throws Exception;
 }
